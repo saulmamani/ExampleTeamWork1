355a51601e0a956544d82f413212a2305249489a
ASSURESOFT\saul.mamani <saul.mamani@trueforce.com>
2023-04-22 07:35:02 -0400

    inicio del trabajo en equipo

---
+++ ConsoleApp1/Operacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Operacion
+    {
+        static public int Sumar(int n1, int n2)
+        {
+            ///TODO trabajo en equipo;
+            return 0;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------EJEMPLO DE TRABAJO EN EQUIPO CON GIT Y GITHUB--------");
+
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"La suma es: { Operacion.Sumar(numero1, numero2) }");
+            Console.WriteLine($"La resta es: {Operacion.Restar(numero1, numero2) }");
+            Console.WriteLine($"La multiplicacion es: { Operacion.Multiplicar(numero1, numero2) }");
+            Console.WriteLine($"La division es: { Operacion.Dividir(numero1, numero2) }");
+
+            Console.ReadKey();
+        }
+    }
+}
